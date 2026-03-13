eec98ee0014df1679b078b71866f6f17d8fa4022
Inés Carazo Núñez <ines.carazo355@comunidadunir.net>
2026-03-13 19:43:52 +0100

    creación de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserFormComponent } from './pages/user-form/user-form.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", pathMatch: 'full', redirectTo: 'home' },
+    { path: "home", component: HomeComponent },
+    { path: "newuser", component: UserFormComponent },
+    { path: "user/:id", component: UserViewComponent },
+    { path: "updateuser/:id", component: UserViewComponent },
+
+    { path: "**", component: HomeComponent }
+];
