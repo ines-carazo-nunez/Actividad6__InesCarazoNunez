dc944656efd43a95288210068babb745cda30bde
Inés Carazo Núñez <ines.carazo355@comunidadunir.net>
2026-03-15 18:54:02 +0100

    Comienzo de la funcionalidad de los btns del grid

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { IUsuario } from '../../interfaces/iusuario.interface';
 import { RouterLink } from '@angular/router';
+import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-user-card',
@@ -9,5 +10,22 @@ import { RouterLink } from '@angular/router';
   styleUrl: './user-card.component.css',
 })
 export class UserCardComponent {
-miUsuario= input<IUsuario>();
+  miUsuario = input<IUsuario>();
+  usuariosService = inject(UsuariosService);
+
+
+  async deleteUser(idUsuario: number | undefined) {
+    if (idUsuario === undefined) {
+      console.error('idUsuario is undefined');
+      return;
+    }
+    const id = Number(idUsuario);
+    if (isNaN(id)) {
+      console.error('idUsuario is not a valid number');
+      return;
+    }
+    const response = await this.usuariosService.deleteUser(id);
+    console.log('aquí');
+    console.log(response);
+  }
 }
