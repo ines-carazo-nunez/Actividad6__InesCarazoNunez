31fdcff3fdf36840c8c363c60da9927777f2bf46
Inés Carazo Núñez <ines.carazo355@comunidadunir.net>
2026-03-15 18:32:32 +0100

    Creado y maquetado el grid de usuarios

--- src/app/pages/user-view/user-view.component.ts
+++ src/app/pages/user-view/user-view.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { UsuariosService } from '../../services/usuarios.service';
+import { IUsuario } from '../../interfaces/iusuario.interface';
 
 @Component({
   selector: 'app-user-view',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './user-view.component.css',
 })
 export class UserViewComponent {
+  id = input<string>()
+  usuariosService = inject(UsuariosService);
+  usuario = signal<IUsuario | null>(null);
 
+
+  async ngOnInit() {
+    const id: number = Number(this.id());
+    //hacemos una petición al servicio
+    this.usuario.set(await this.usuariosService.getByID(id))
+    console.log(this.usuario());
+    
+  }
 }
