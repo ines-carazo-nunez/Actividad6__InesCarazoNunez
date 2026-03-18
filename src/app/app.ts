e577d5aaef019874008e769f3920a4f7f75a1928
Inés Carazo Núñez <ines.carazo355@comunidadunir.net>
2026-03-18 11:25:07 +0100

    Funcionalidad de crear usuario terminada
    peticiones CRUD en el servicio, createUser terminado y comienzo de updateUser

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/header/header.component';
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent ],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/pages/user-form/user-form.component.ts
+++ src/app/pages/user-form/user-form.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IUsuario } from '../../interfaces/iusuario.interface';
+import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-user-form',
@@ -8,9 +10,29 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './user-form.component.css',
 })
 export class UserFormComponent {
- userForm: FormGroup
+  userForm: FormGroup
+  usuariosServices = inject(UsuariosService);
+  id = input<string>()
+  //productosService = inject(ProductosService)
+  usuario = signal<IUsuario | undefined>(undefined)
+  isUpdateMode: boolean = false;
 
- constructor() {
+  async ngOnInit() {
+    const idUsuario: string = this.id()!;
+
+    this.isUpdateMode = !!this.id; // Si hay id, isUpdateMode es true
+
+    if (this.isUpdateMode) {
+      //hacemos una petición al servicio
+      this.usuario.set(await this.usuariosServices.getByID(idUsuario));;
+      console.log(this.usuario());
+    }
+
+  }
+
+  constructor() {
+    console.log(this.id());
+
     this.userForm = new FormGroup({
       nombre: new FormControl("", [
         Validators.required
@@ -22,19 +44,35 @@ export class UserFormComponent {
         Validators.required,
         Validators.pattern(/^\w+\@[a-zA-Z_]+?\.[a-zA-Z_]{2,3}/)
       ]),
-      image: new FormControl("", [
+      urlimage: new FormControl("", [
         Validators.required
       ]),
     }, [])
   }
 
-checkControl(controlName: string, errorName: string): boolean | undefined {
+  checkControl(controlName: string, errorName: string): boolean | undefined {
     return this.userForm.get(controlName)?.hasError(errorName) && this.userForm.get(controlName)?.touched
   }
 
-  getDataForm() {
-    console.log(this.userForm.value);
-    this.userForm.reset()
+  async getDataForm() {
+    if (this.isUpdateMode) {
+      // Llamar a servicio.actualizar(id, datos)
+    } else {
+      // Llamar a servicio.crear(datos)
+      console.log(this.userForm.value.urlimage);
+      let usuario: IUsuario = {
+        first_name: this.userForm.value.nombre,
+        last_name: this.userForm.value.apellido,
+        username: this.userForm.value.nombre + this.userForm.value.apellido,
+        email: this.userForm.value.email,
+        image: this.userForm.value.urlimage,
+        password: '12345'
+      }
+      const response: IUsuario = await this.usuariosServices.createUser(usuario);
+      console.log(response);
+      alert('Usuario creado con éxito');
+      this.userForm.reset()
+    }
 
   }
 }
